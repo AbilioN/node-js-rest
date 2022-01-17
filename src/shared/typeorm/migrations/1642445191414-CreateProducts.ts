253b2f42177797509c0ada0f469f593bbeda8dc5
AbilioN <netobalby@gmail.com>
2022-01-17 19:44:06 -0300

    infra: added migration for product in mysql version

---
+++ src/shared/typeorm/migrations/1642445191414-CreateProducts.ts
@@ -0,0 +1,59 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import {v4 as uuidV4 } from 'uuid';
+
+export class CreateProducts1642445191414 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+        // await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid"');
+        await queryRunner.createTable(new Table({
+            name : "products",
+            columns : [
+                {
+                    name: 'id',
+                    type: 'varchar',
+                    isPrimary : true,
+                },
+                // {
+                //     name: 'id',
+                //     type: 'uuid',
+                //     isPrimary : true,
+                //     generationStrategy : 'uuid',
+                //     default : 'uuid_generate_v4()'
+                // },
+                {
+                    name : 'name',
+                    type : 'varchar',
+
+                },
+                {
+                    name : 'price',
+                    type : 'decimal',
+                    precision : 10,
+                    scale : 2,
+
+                },
+                {
+                    name : 'created_at',
+                    type : 'timestamp',
+                    default : 'now()',
+
+                },
+                {
+                    name : 'updated_at',
+                    type : 'timestamp',
+                    default : 'now()',
+                    
+                }
+
+
+            ]
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.dropTable('products');
+    }
+
+}
