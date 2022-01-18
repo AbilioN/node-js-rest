2f392c4b3b4a5796f5f36705d1e2c2ef014b2f30
AbilioN <netobalby@gmail.com>
2022-01-17 23:16:21 -0300

    feature : added all Product services

---
+++ src/modules/products/services/ListProductService.ts
@@ -0,0 +1,25 @@
+import AppError from "@shared/errors/AppError";
+import { getCustomRepository } from "typeorm";
+import Product from "../typeorm/entities/Product";
+import { ProductRepository } from "../typeorm/repositories/ProductsRepository";
+
+interface IRequest 
+{
+    name: string
+    price: number
+    quantity: number
+}
+
+class ListProductService
+{
+    public async execute() : Promise<Product[]>
+    {
+        const productsRepository = getCustomRepository(ProductRepository);
+
+        const products  = productsRepository.find();
+
+        return products;
+    }
+}
+
+export default ListProductService;
