29f46cbf79725b1ed0728392511e08422b271b11
AbilioN <netobalby@gmail.com>
2022-01-20 21:30:41 -0300

    feature : add view parse string for email

--- src/config/mail/EtherealMail.ts
+++ src/config/mail/EtherealMail.ts
@@ -6,7 +6,7 @@ interface ITemplateVariable {
     [key: string] : string | number;
 }
 interface IParseMailTemplate {
-    template : string,
+    file : string,
     variables :  ITemplateVariable
 }
 interface IMailContact {
--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from "typeorm";
 import UsersRepository from "../typeorm/repositories/UsersRepository";
 import UserTokensRepository from "../typeorm/repositories/UserTokensRepository";
 import EtherealMail from '@config/mail/EtherealMail';
+import path from 'path';
 interface IRequest 
 {
     email: string
@@ -26,8 +27,7 @@ class SendForgotPasswordEmailService
 
         const token = await userTokensRepository.generate(user.id);
 
-        console.log(token);
-        
+        const forgotPasswordTemplate = path.resolve(__dirname, '..' , 'views' , 'forgot_password.hbs');
         await EtherealMail.sendEmail({
             to: {
                 name : user.name,
@@ -35,10 +35,10 @@ class SendForgotPasswordEmailService
             },
             subject : 'Api type password recorvery',
             templateData : {
-                template : `Ola {{name}}  : {{token}}`,
+                file : forgotPasswordTemplate,
                 variables : {
                     name : user.name,
-                    token : token?.token,
+                    link : `http://localhost:3000/reset_password?token=${token.token}`,
                 }
             }
         });
